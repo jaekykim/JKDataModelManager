5767cecd4025761ac368c461d5ae251bbded9b1a
Jaekyong Kim <jaekyong@vitalhub.com>
2015-06-29 11:46:57 -0400

    Test File

---
+++ Error/Error400.h
@@ -0,0 +1,13 @@
+//
+//  Error400.h
+//  JKDataModelManager
+//
+//  Created by Jae Kim on 2015-06-29.
+//  Copyright (c) 2015 Jaekyong Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Error400 : NSObject
+
+@end
---
+++ Error/Error400.m
@@ -0,0 +1,13 @@
+//
+//  Error400.m
+//  JKDataModelManager
+//
+//  Created by Jae Kim on 2015-06-29.
+//  Copyright (c) 2015 Jaekyong Kim. All rights reserved.
+//
+
+#import "Error400.h"
+
+@implementation Error400
+
+@end
