62a1a158677d3fee2774a82d0953c9d1af68f470
Jaekyong Kim <jaekyong@vitalhub.com>
2015-06-29 11:00:18 -0400

    Initial Commit

---
+++ JKDataModelManager/JKDataModelManager.m
@@ -0,0 +1,13 @@
+//
+//  JKDataModelManager.m
+//  JKDataModelManager
+//
+//  Created by Jae Kim on 2015-06-29.
+//  Copyright (c) 2015 Jaekyong Kim. All rights reserved.
+//
+
+#import "JKDataModelManager.h"
+
+@implementation JKDataModelManager
+
+@end
